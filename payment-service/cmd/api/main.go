9d46123ad257cd8f10576c031bce51d8e7812dc7
dhananjay sharma <dhananjayksharma@gmail.com>
2026-02-17 18:41:34 +0530

    added pem mysql cert

--- payment-service/cmd/api/main.go
+++ payment-service/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -13,11 +14,15 @@ import (
 func main() {
 	dsn := os.Getenv("MYSQL_DSN")
 	if dsn == "" || len(dsn) == 0 {
-		dsn = "root:root@tcp(localhost:3306)/appdb?parseTime=true"
 		log.Fatalf("dsn string not found error: %v", dsn)
 	}
 
-	gdb, err := db.NewGormMySQL(dsn)
+	fmt.Println("dsn::", dsn)
+	capempath := os.Getenv("MYSQL_DBPEM")
+	if capempath == "" || len(capempath) == 0 {
+		log.Fatalf("capempath string not found error: %v", capempath)
+	}
+	gdb, err := db.ConnectMySQL(dsn, capempath)
 	if err != nil {
 		log.Fatalf("db connect error: %v", err)
 	}
---
+++ payment-service/internal/db/mysql-client.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
+
+	mysqlDriver "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectMySQL(dsn string, capempath string) (*gorm.DB, error) {
+	caCert, err := os.ReadFile(capempath)
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	if ok := certPool.AppendCertsFromPEM(caCert); !ok {
+		panic("Failed to append CA cert")
+	}
+
+	// 2. Create TLS config
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	// Register TLS config
+	err = mysqlDriver.RegisterTLSConfig("custom", tlsConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// 3. MySQL URL (DSN)
+	// Format:
+	// user:password@tcp(host:3306)/dbname?tls=custom&parseTime=true
+	// fmt.Println("dsn:::", dsn)
+	// 4. Open GORM connection
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("✅ connected with CA TLS")
+	return db, nil
+}
